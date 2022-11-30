bc4cc48b7031353b9da63ab70fe5c27e954e588f
dennychandr <57188574+dennychandr@users.noreply.github.com>
2022-11-30 22:27:23 +0700

    dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,21 +1,68 @@
 //Hamburger
 
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const hamburgerLine1 = document.querySelector("#hamburger-line-1");
+const hamburgerLine2 = document.querySelector("#hamburger-line-2");
+const hamburgerLine3 = document.querySelector("#hamburger-line-3");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
-})
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
 
 //Navbar Fixed
-window.onscroll = function(){
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
-    if(window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed');
-    }else{
-        header.classList.remove('navbar-fixed');
-    }
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
+  }
+};
+
+// Klik di luar hamburger
+window.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (
+    e.target != hamburger &&
+    e.target != navMenu &&
+    e.target != hamburgerLine1 &&
+    e.target != hamburgerLine2 &&
+    e.target != hamburgerLine3
+  ) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+//Darkmode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+darkToggle.addEventListener("click", () => {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+//Pindahkan posisi toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ["./index.html"],
+  darkMode: "class",
   theme: {
-    container:{
-      center:true,
-      padding: '16px',
+    container: {
+      center: true,
+      padding: "16px",
     },
     extend: {
-      colors:{
-        primary: '#06b6d4',
-        secondary: '#64748b',
-        dark: '#1e293b',
-        instagram: '#E4405F',
-        line: '#00C300',
-        whatsapp: '#25D366',
-        twitter: '#1DA1F2',
-        tiktok: '#000000',
-        linkedin: '#0A66C2',
-        discord: '#5865F2',
-        tailwind: '#0ea5e9',
+      colors: {
+        primary: "#06b6d4",
+        secondary: "#64748b",
+        light: "#e2e8f0",
+        dark: "#0f172a",
+        instagram: "#E4405F",
+        line: "#00C300",
+        whatsapp: "#25D366",
+        twitter: "#1DA1F2",
+        tiktok: "#000000",
+        linkedin: "#0A66C2",
+        discord: "#5865F2",
+        tailwind: "#0ea5e9",
       },
-      screens:{
-        '2xl': '1320px',
-      }
+      screens: {
+        "2xl": "1320px",
+      },
     },
   },
-  plugins: [require('prettier-plugin-tailwindcss')],
-  tailwindConfig: './styles/tailwind.config.js',
-}
+  plugins: [],
+};
